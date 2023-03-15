43df2f7da6b8ff373a381e2d44ab531b37213d88
Jakub Lepik <kuba.lepik@seznam.cz>
2023-03-15 23:32:09 +0100

    IsThere, isSunk, isHit...

--- ShipBattle/Data/Ship.cs
+++ ShipBattle/Data/Ship.cs
@@ -4,20 +4,19 @@
     {
         public enum ShipType : int
         {
-            Carrier = 5,
-            Battleship = 4,
-            Cruiser = 3,
-            Submarine = Cruiser,
-            Destroyer = 2,
+            Carrier,
+            Battleship,
+            Cruiser,
+            Submarine,
+            Destroyer,
         }
-
         private ShipType Type { get; set; }
 
         private int OriginCol { get; set; }
-        private int OriginRow { get; set; }
+        private int OriginRow { get; }
         private bool Rotate { get; set; }
         
-        
+        private int Hits { get; set; }
 
         public Ship(ShipType type, int origincol, int originrow, bool rotate)
         {
@@ -25,6 +24,7 @@
             OriginCol = origincol;
             OriginRow = originrow;
             Rotate = rotate;
+            Hits = 0;
         }
         
         public string CssOriginCol()
@@ -43,7 +43,7 @@
             }
             return "0deg";
         }
-        public string CssShipName()
+        public string Name()
         {
             if (Type == ShipType.Carrier) { return "Carrier"; }
             if (Type == ShipType.Battleship) { return "Battleship"; }
@@ -51,6 +51,38 @@
             if (Type == ShipType.Submarine) { return "Submarine"; }
             return "Destroyer";
         }
-
+        public int Length()
+        {
+            if (Type == ShipType.Carrier) { return 5; }
+            if (Type == ShipType.Battleship) { return 4; }
+            if (Type == ShipType.Cruiser) { return 3; }
+            if (Type == ShipType.Submarine) { return 3; }
+            return 2;
+        }
+        public bool IsHit(int col, int row)
+        {
+            if (IsThere(col, row))
+            {
+                Hits++;
+                return true;
+            }
+            return false;
+        }
+        public bool IsThere(int col, int row)
+        {
+            if (Rotate)
+            {
+                if (row != OriginRow) { return false; }
+                if (col >= OriginCol && col <= OriginCol + Length()) { return true; }
+                return false;
+            }
+            if (col != OriginCol) { return false; }
+            if (row >= OriginRow && row <= OriginRow + Length()) { return true; }
+            return false;
+        }
+        public bool IsSunk()
+        {
+            return (Hits == Length());
+        }
     }
 }
