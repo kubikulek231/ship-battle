ac998a1fcfd554c6b07e23abfbf3668278a3b2e2
kubikulek231 <kuba.lepik@seznam.cz>
2023-04-02 19:18:19 +0200

    from absolute pos to grid, new AddShip verification, RandArr class

---
+++ ship-battle/Data/PlayerBoard.cs
@@ -0,0 +1,98 @@
+﻿namespace ship_battle.Data
+{
+    public class PlayerBoard
+    {
+        public int Player { get; }
+        public enum SquareType
+        {
+            Unknown,
+            Hit,
+            Miss,
+            NewHit,
+            NewMiss,
+        }
+        const int BOARD_SIZE = 10;
+        private SquareType[,] HitRecord;
+        public List<Ship> Ships { get; private set; }
+        public PlayerBoard(int player)
+        {
+            Player = player;
+            HitRecord = new SquareType[BOARD_SIZE, BOARD_SIZE];
+            for (int i = 0; i < BOARD_SIZE; i++)
+            {
+                for (int j = 0; j < BOARD_SIZE; j++)
+                {
+                    HitRecord[i, j] = SquareType.Unknown;
+                }
+            }
+            Ships = new List<Ship>();
+        }
+        public bool AddShip(Ship ship)
+        {
+            if (Ships.Contains(ship))
+            {
+                return false;
+            }
+            if (!IsValidNewShipLocation(ship))
+            {
+                return false;
+            }
+            Ships.Add(ship);
+            return true;
+
+        }
+        public SquareType Hit(int x, int y)
+        {
+            if (HitRecord[x, y] != SquareType.Unknown)
+            {
+                return HitRecord[x, y];
+            }
+            foreach (Ship ship in Ships)
+            {
+                if (ship.IsHit(x, y))
+                {
+                    HitRecord[x, y] = SquareType.Hit;
+                    return SquareType.NewHit;
+                }
+            }
+            HitRecord[x, y] = SquareType.Miss;
+            return SquareType.NewMiss;
+        }
+        public Ship GetShip(int x, int y)
+        {
+            foreach (Ship ship in Ships)
+            {
+                if (ship.IsThere(x, y))
+                {
+                    return ship;
+                }
+            }
+            throw new Exception("No ship found at specified coordinates.");
+        }
+        private bool IsValidNewShipLocation(Ship new_ship)
+        {
+            if (!IsInBounds(new_ship)) { return false; }
+            foreach (Ship ship in Ships)
+            {
+                if (ship.DoesCollide(new_ship)) { return false; }
+            }
+            return true;
+        }
+        private static bool IsInBounds(Ship ship)
+        {
+            if (ship.Rotate)
+            {
+                if (ship.OriginCol + ship.Length() - 1 > 9)
+                {
+                    return false;
+                }
+                return true;
+            }
+            if (ship.OriginRow + ship.Length() - 1 > 9)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ ship-battle/Data/Ship.cs
@@ -0,0 +1,119 @@
+﻿namespace ship_battle.Data
+{
+    public class Ship
+    {
+        public enum ShipType
+        {
+            Carrier,
+            Battleship,
+            Cruiser,
+            Submarine,
+            Destroyer,
+        }
+        private ShipType Type { get; set; }
+        public int OriginCol { get; private set; }
+        public int OriginRow { get; private set; }
+        public int EndCol { get; private set; }
+        public int EndRow { get; private set; }
+
+        public int OriginCol1Based
+        {
+            get { return OriginCol + 1; }
+        }
+
+        public int OriginRow1Based
+        {
+            get { return OriginRow + 1; }
+        }
+        public int EndCol1Based
+        {
+            get { return EndCol + 1; }
+        }
+        public int EndRow1Based
+        {
+            get { return EndRow + 1; }
+        }
+
+        public bool Rotate { get; }
+
+        private int Hits { get; set; }
+
+        public Ship(ShipType type, int origincol, int originrow, bool rotate)
+        {
+            Type = type;
+            OriginCol = origincol;
+            OriginRow = originrow;
+            Rotate = rotate;
+            EndCol = !rotate ? origincol : origincol + Length();
+            EndRow = !rotate ? originrow + Length() : originrow;
+            Hits = 0;
+        }
+
+        public string Name()
+        {
+            if (Type == ShipType.Carrier) { return "Carrier"; }
+            if (Type == ShipType.Battleship) { return "Battleship"; }
+            if (Type == ShipType.Cruiser) { return "Cruiser"; }
+            if (Type == ShipType.Submarine) { return "Submarine"; }
+            return "Destroyer";
+        }
+        public int Length()
+        {
+            if (Type == ShipType.Carrier) { return 5; }
+            if (Type == ShipType.Battleship) { return 4; }
+            if (Type == ShipType.Cruiser) { return 3; }
+            if (Type == ShipType.Submarine) { return 3; }
+            return 2;
+        }
+        public bool IsHit(int col, int row)
+        {
+            if (IsThere(col, row))
+            {
+                Hits++;
+                return true;
+            }
+            return false;
+        }
+        public bool IsThere(int col, int row)
+        {
+            if (col > 9 || row > 9)
+            {
+                return false;
+            }
+
+            if (Rotate)
+            {
+                if (row != OriginRow) { return false; }
+                if (col >= OriginCol && col < OriginCol + Length()) { return true; }
+                return false;
+            }
+            if (col != OriginCol) { return false; }
+            if (row >= OriginRow && row < OriginRow + Length()) { return true; }
+            return false;
+        }
+        public bool IsSunk()
+        {
+            return (Hits == Length());
+        }
+
+        public bool DoesCollide(Ship ship)
+        {
+            for (int i = 0; i < Length(); i++)
+            {
+                if (Rotate)
+                {
+                    if (ship.IsThere(OriginCol + i, OriginRow))
+                    {
+                        return true;
+                    }
+                    continue;
+                }
+                if (ship.IsThere(OriginCol, OriginRow + i))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
